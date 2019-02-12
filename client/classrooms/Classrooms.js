dfe36bb2530510cd7dc2131e3a8a3b518b52dd48
GuillN <gnaassan@gmail.com>
2019-02-12 11:39:49 +0100

    show/hide new classroom

--- client/classrooms/Classrooms.js
+++ client/classrooms/Classrooms.js
@@ -2,6 +2,20 @@ Meteor.subscribe('classrooms');
 
 Template.Classrooms.helpers({
     classrooms: () => {
+        Session.set('newClassroomHidden',true);
         return Classrooms.find({})
+    },
+    newClassroom:function(){
+        return Session.get('newClassroomHidden')
+    }
+});
+
+Template.Classrooms.events({
+    'click #showNew':function(){
+        Session.set('newClassroomHidden',false);
+        console.log(Session)
+    },
+    'click #hideNew':function(){
+        Session.set('newClassroomHidden',true);
     }
 });
