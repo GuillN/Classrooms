1d3cf4b4df0900d44d838600f55f298e0b1a8fbd
GuillN <gnaassan@gmail.com>
2019-03-07 23:34:54 +0100

    blu

--- server/main.js
+++ server/main.js
@@ -1,5 +1,16 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+    // code to run on server at startup
+    //   console.log(process.env.MONGO_URL);
+    // const MongoClient = require('mongodb').MongoClient;
+    // const uri = "mongodb+srv://gnaassan:root@classroom-bx0ks.mongodb.net/test?retryWrites=true";
+    // const client = new MongoClient(uri, { useNewUrlParser: true });
+    // client.connect(err => {
+    //     console.log('Connected to mongodb');
+    //     const collection = client.db("Classroom").collection("classrooms");
+    //     // perform actions on the collection object
+    //     client.close();
+    // });
+
 });
